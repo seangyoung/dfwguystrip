63625e9d1db2f050d477f6b85d134fd3d1d87b1e
Sean G Young <seangyoung@iMac.local>
2026-08-29 22:11:09 -0500

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/dfw-guys-trip-hub/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/dfwguystrip/' : '/',
   plugins: [react()],
 })
